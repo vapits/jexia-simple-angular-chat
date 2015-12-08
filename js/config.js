a75c9b929a8dfcf9277665a4afc7cfd86a0d8ca5
Vassilis <vapits@gmail.com>
2015-12-08 13:22:46 +0100

    add datasets in config

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ var jexia = {
     appId: '47a4ec70-9c47-11e5-be62-df2d328a133b',	// Jexia Data App Id
 	appUrl: 'https://47a4ec70-9c47-11e5-be62-df2d328a133b.app.jexia.com/',	// Jexia Data App Url
 	key: 'ad316b44e3b4349a3bbb325b49b31eab', // Data App Access Key (key)
-	secret: 'cc035a7ccaab5650413db60c73ec2e81c807b8e8890b5d5e' // Data App Access Key (secret)
+	secret: 'cc035a7ccaab5650413db60c73ec2e81c807b8e8890b5d5e', // Data App Access Key (secret)
+	usersDataSet: 'users', // The Data Set that users logs are saved
+	feedDataSet: 'feed' // The Data Set that messages saved
 
 };
