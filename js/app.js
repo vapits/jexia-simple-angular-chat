3cea15ccfa457d5bebb9813cd4818d34b0e51c52
Vassilis <vapits@gmail.com>
2015-12-09 11:08:27 +0100

    add interceptors to check responseErrors

--- js/app.js
+++ js/app.js
@@ -12,7 +12,24 @@ angular.module('simpleChat', [
 
 })
 
-.config(function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider, $httpProvider) {
+
+	// If token or credentials are wrong in any request then 
+	// send user to login page
+	$httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
+        return {
+            'responseError': function(response) {
+                if (response.status === 403) {
+                    localStorage.removeItem('username');
+					localStorage.removeItem('userId');
+					localStorage.removeItem('token');
+                    $location.path('/login');
+                }
+
+                return $q.reject(response);
+            }
+        };
+    }]);
 	
 	// Now let's set two simple routes for our views
 	// a login page and the chat rooom
