8542c5d38b57564e774a8086d9de1f3a375c29bc
Vassilis <vapits@gmail.com>
2015-12-14 10:21:58 +0100

    clean up credentials

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var jexia = {
 
-    appId: '47a4ec70-9c47-11e5-be62-df2d328a133b',	// Jexia Data App Id
-	appUrl: 'http://47a4ec70-9c47-11e5-be62-df2d328a133b.app.jexia.com/',	// Jexia Data App Url
-	key: 'ad316b44e3b4349a3bbb325b49b31eab', // Data App Access Key (key)
-	secret: 'cc035a7ccaab5650413db60c73ec2e81c807b8e8890b5d5e', // Data App Access Key (secret)
+    appId: '',	// Jexia Data App Id
+	appUrl: '',	// Jexia Data App Url (keep the / at the end)
+	key: '', // Data App Access Key (key)
+	secret: '', // Data App Access Key (secret)
 	usersDataSet: 'users', // The Data Set that users logs are saved
 	feedDataSet: 'feed' // The Data Set that messages saved
 
